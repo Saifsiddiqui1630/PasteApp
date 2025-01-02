7c826041127f82e48dde1a2651958073cab45686
Saif Siddiqui <161847569+Saifsiddiqui1630@users.noreply.github.com>
2025-01-02 17:12:49 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div className='flex  border w-[700px] h-[40px]  bg-green-200 text- text-shadow-lg font-semibold '>
+    <div className='flex  border w-[full] h-[40px]  bg-green-200 text- text-shadow-lg font-semibold '>
       <NavLink
         to={"/"} className='hover:underline hover:bg-green-500 h-full w-[85px] border-r flex justify-center items-center active:bg-green-500 rounded'
       >
